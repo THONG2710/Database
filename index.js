b59e59432a0342364ded1e64143d5b111ebe12d3
Nguyen Van Thong <5506nguyenthong@gmail.com>
2024-02-13 16:49:00 +0700

    commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// session, cookie
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
