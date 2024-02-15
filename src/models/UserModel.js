d40b493178479e607ca00625a02af55e2e5483d5
LILLINH <102342993+LILLINH@users.noreply.github.com>
2024-02-15 23:06:52 +0700

    update

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,15 +5,13 @@ const ObjectId = Schema.ObjectId;
 const userSchema = new Schema({
     _id: { type: ObjectId },
     password: { type: String },
-    fullName: { type: String },
     email: { type: String },
-    address: { type: String },
-    cccd: { type: String},
-    phoneNumber: { type: String },
-    score: { type: Number },
-    role: { type: Number },
-    gender: { type: Number },
-    avatar: { type: String },
+    friend: { type: Array },
+    role: { type: String },
+    isLooked : { type: Boolean },
+    time_create: { type: Date },
+    friend_request: { type: Array },
+    request_to_be_friend: { type: Array },
 });
 
 module.exports = mongoose.model('users', userSchema);
